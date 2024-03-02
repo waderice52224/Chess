b4fc449adf1655d269be3e1a2531666ee54071b9
waderice52224 <waderice52224@gmail.com>
2024-03-01 19:33:32 -0700

    Added peice classes boilerplate

--- peices.js
+++ peices.js
@@ -1,5 +1,37 @@
-class Board {
-    constructor() {
-      this.square = [];
+class Peices {
+    constructor(color, type) {
+      this.color = color;
+      this.type = type;
+      this.isAlive = true;
+    }
+  }
+  class Pawn extends Peices{
+    constructor(){
+      super(color, color, this.isAlive);
+    }
+  }
+  class Rook extends Peices{
+    constructor(){
+      super(color, color, this.isAlive);
+    }
+  }
+  class knight extends Peices{
+    constructor(){
+      super(color, color, this.isAlive);
+    }
+  }
+  class Bishop extends Peices{
+    constructor(){
+      super(color, color, this.isAlive);
+    }
+  }
+  class Queen extends Peices{
+    constructor(){
+      super(color, color, this.isAlive);
+    }
+  }
+  class King extends Peices{
+    constructor(){
+      super(color, color, this.isAlive);
     }
   }
